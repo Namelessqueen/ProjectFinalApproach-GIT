c6e66f77c6284622e5d82d84c084961f62708708
Timid_ <151643430+Timid05@users.noreply.github.com>
2024-05-07 14:40:09 +0200

    Tiled implementation added

--- ProjectFinalApproach-GXP/GXPEngine/MyGame.cs
+++ ProjectFinalApproach-GXP/GXPEngine/MyGame.cs
@@ -1,31 +1,48 @@
-using System;                                   // System contains a lot of default C# libraries 
-using GXPEngine;                                // GXPEngine contains the engine
-using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System;                                   
+using GXPEngine;                                
+using System.Drawing;
+using System.Collections.Generic;
+using System.Web;
 
 public class MyGame : Game {
-	public MyGame() : base(1032, 1366, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		Vec2 newVec = new Vec2();
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.Aqua);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(game.width / 2, game.height / 2, 100, 100);
-		canvas.Fill(50);
-		canvas.TextSize(20);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Planet", width / 2, height / 2);
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
+	string nextScene = null;
+
+	public MyGame() : base(800, 600, false)    
+	{
+	    OnAfterStep += CheckLoadScene;
+		LoadScene("game.tmx");
+	}
+	
+	void DestroyAll()
+	{
+		List<GameObject> children = GetChildren();
+		foreach (GameObject child in children)
+		{
+			child.Destroy();
+		}
 	}
 
-	// For every game object, Update is called every frame, by the engine:
+	public void LoadScene(string filename)
+	{
+		nextScene = filename;
+	}
+
+	void CheckLoadScene()
+	{
+		if (nextScene != null)
+		{
+			DestroyAll();
+			AddChild(new Scene(nextScene));
+			nextScene = null;
+		}
+	}
+
 	void Update() {
 	
 	}
 
-	static void Main()                          // Main() is the first method that's called when the program is run
+	static void Main()                          
 	{
 		new MyGame().Start();                  
 	}
