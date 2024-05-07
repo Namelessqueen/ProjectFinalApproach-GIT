374395a433b22c56410639ffd8dbc5e0a5aa73d0
Timid_ <151643430+Timid05@users.noreply.github.com>
2024-05-07 10:12:48 +0200

    Completed Vec2 struct added

--- ProjectFinalApproach-GXP/GXPEngine/MyGame.cs
+++ ProjectFinalApproach-GXP/GXPEngine/MyGame.cs
@@ -7,26 +7,25 @@ public class MyGame : Game {
 	{
 		// Draw some things on a canvas:
 		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
+		canvas.Clear(Color.Aqua);
 		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
+		canvas.Ellipse(game.width / 2, game.height / 2, 100, 100);
 		canvas.Fill(50);
-		canvas.TextSize(32);
+		canvas.TextSize(20);
 		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
+		canvas.Text("Planet", width / 2, height / 2);
 
 		// Add the canvas to the engine to display it:
 		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
 	}
 
 	// For every game object, Update is called every frame, by the engine:
 	void Update() {
-		// Empty
+	
 	}
 
 	static void Main()                          // Main() is the first method that's called when the program is run
 	{
-		new MyGame().Start();                   // Create a "MyGame" and start it
+		new MyGame().Start();                  
 	}
 }
